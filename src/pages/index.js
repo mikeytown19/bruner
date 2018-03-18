eafd1e1c16b23a94279144a865197d2a32e0fab1
Michael Harvey <mikeytown19@gmail.com>
2018-03-18 12:45:45 -0600

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,25 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Script from 'react-load-script'
 
 export default class IndexPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  handleScriptLoad() {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on('init', user => {
+        if (!user) {
+          window.netlifyIdentity.on('login', () => {
+            document.location.href = '/admin/'
+          })
+        }
+      })
+    }
+    window.netlifyIdentity.init()
+  }
+
   render() {
     const { data } = this.props
     const { edges: posts } = data.allMarkdownRemark
@@ -38,6 +56,11 @@ export default class IndexPage extends React.Component {
               </div>
             ))}
         </div>
+        <Script
+          url="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          onLoad={this.handleScriptLoad.bind(this)}
+        />
+
       </section>
     )
   }
